2a5776292c46b04efcb315c2bd47973999449f8b
Rob Tade <robtade4@gmail.com>
2026-01-12 21:56:41 +0300

    Add UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -17,6 +18,11 @@ function App() {
     <Header />
     <MainContent />
     <Footer />
+    <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

8b42f7c227338558134aa3988ee9f8801cd935fa
Rob Tade <robtade4@gmail.com>
2026-01-25 23:12:54 +0300

    add color to texts

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "15px",
+        marginTop: "20px",
+      }}
+    >
+      <p style={{ margin: 0 }}>
+        © 2026 My Favorite Cities. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#e0f7fa",
+        minHeight: "200px",
+        fontFamily: "Verdana, sans-serif",
+      }}
+    >
+      <p style={{ fontSize: "1.1em", lineHeight: "1.6", color: "#333" }}>
+        Welcome to the main content area. Here you can showcase your favorite
+        cities and travel experiences.
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;

15164b602407d4d3339df4cd2a01caf337e7e793
Rob Tade <robtade4@gmail.com>
2026-01-25 04:06:20 +0300

    Add DeleteRecipe, EditRecipe, and RecipeDetails

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,25 @@
-import AddRecipeForm from "./components/AddRecipeForm";
-import RecipeList from "./components/RecipeList";
+import { Routes, Route } from "react-router-dom";
+import AddRecipeForm from "./components/AddRecipeForm.jsx";
+import RecipeList from "./components/RecipeList.jsx";
+import RecipeDetails from "./components/RecipeDetails.jsx";
 
 function App() {
   return (
     <div style={{ padding: "1rem" }}>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }

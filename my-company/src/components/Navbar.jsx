0cd68f3c9a81a9adf3cbad27ab014c6ee01ae0e7
Rob Tade <robtade4@gmail.com>
2026-01-25 23:49:59 +0300

    update navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,33 +4,23 @@ function Navbar() {
   return (
     <nav
       style={{
+        display: "flex", // flex container
+        justifyContent: "center", // center links horizontally
         backgroundColor: "#003366",
         padding: "10px",
-        textAlign: "center",
+        gap: "15px", // spacing between links
       }}
     >
-      <Link
-        to="/"
-        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}
-      >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
         Home
       </Link>
-      <Link
-        to="/about"
-        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}
-      >
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
         About
       </Link>
-      <Link
-        to="/services"
-        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}
-      >
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>
         Services
       </Link>
-      <Link
-        to="/contact"
-        style={{ color: "white", margin: "0 15px", textDecoration: "none" }}
-      >
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
         Contact
       </Link>
     </nav>

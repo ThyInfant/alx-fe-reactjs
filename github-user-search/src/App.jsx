55727dc24f784b2ab9fab311b0cd53f202e956c2
Rob Tade <robtade4@gmail.com>
2026-02-01 13:25:46 +0300

    Initial React setip with GitHub API integration

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>GitHub User Search</h1>
+      <p>Welcome to the GitHub User Search application!</p>
+    </div>
+  );
+}
+
+export default App;

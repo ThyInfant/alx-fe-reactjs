0d65dea0cfce1bf4662781238ba9bf0e320b8792
Rob Tade <robtade4@gmail.com>
2026-02-01 13:43:21 +0300

    Change code approach in githubServices

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,16 @@
-import githubApi from "./githubApi";
 import axios from "axios";
+import githubApi from "./githubApi"; // still allowed, even if not used directly
+
+const BASE_URL = "https://api.github.com";
 
 export const fetchUserData = async (username) => {
-  const response = await githubApi.get(`/users/${username}`);
+  const response = await axios.get(`${BASE_URL}/users/${username}`, {
+    headers: {
+      Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+        ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+        : "",
+    },
+  });
+
   return response.data;
 };

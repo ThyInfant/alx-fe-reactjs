52d44d575d8b1bd8c9bdfbf99c0f546ac692f153
Rob Tade <robtade4@gmail.com>
2026-02-17 00:49:08 +0300

    Add explicit mobile grid column for responsive layout

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,7 +17,7 @@ export default function HomePage() {
       </h1>
 
       {/* Responsive Grid */}
-      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}

16a5df349a04228dc93157ebc44a3d48bce767fc
Rob Tade <robtade4@gmail.com>
2026-02-21 01:35:04 +0300

    Implemented React Router with nested routes (ProfileDetails, ProfileSettings), dynamic routes (BlogPost/:id), and protected routes (Profile). Added basic authentication and navigation.

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const App = () => {
+  return (
+    <div>
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/123">Blog Post</Link> | <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
